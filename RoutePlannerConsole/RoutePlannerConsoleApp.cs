318680b33249c3fa5363dc73efe9517ba547819b
the-s-a-m <s.merki@hotmail.com>
2015-02-16 10:43:24 +0100

    initial commit

---
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
+{
+    class RoutePlannerConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to RoutePlanner (Version " +
+                System.Reflection.Assembly.GetExecutingAssembly().GetName().Version + 
+                ") ");
+            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude); 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RoutePlannerLib/WayPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class WayPoint
+    {
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+
+        public WayPoint(string _name, double _laltitude, double _longitude)
+        {
+            Name = _name;
+            Latitude = _laltitude;
+            Longitude = _longitude;
+        }
+    }
+}
