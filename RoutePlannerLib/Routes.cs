68398b36e8c0380e526c72b5e7de36e579d75260
the-s-a-m <s.merki@hotmail.com>
2015-03-08 15:40:25 +0100

    last changes

---
+++ RoutePlannerLib/Routes.cs
@@ -0,0 +1,78 @@
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    /// <summary>
+    /// Manages a routes from a city to another city.
+    /// </summary>
+    public class Routes
+    {
+        private List<Link> routes = new List<Link>();
+        private Cities cities;
+        public delegate void RouteRequestHandler(object sender, RouteRequestEventArgs e);
+        public event RouteRequestHandler RouteRequestEvent;
+
+        public int Count
+        {
+            get { return routes.Count; }
+        }
+
+        /// <summary>
+        /// Initializes the Routes with the cities.
+        /// </summary>
+        /// <param name="cities"></param>
+        public Routes(Cities cities)
+        {
+            this.cities = cities;
+        }
+
+        /// <summary>
+        /// Reads a list of links from the given file.
+        /// Reads only links where the cities exist.
+        /// </summary>
+        /// <param name="filename">name of links file</param>
+        /// <returns>number of read route</returns>
+        public int ReadRoutes(string filename)
+        {
+            using (TextReader reader = new StreamReader(filename))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var linkAsString = line.Split('\t');
+
+                    City city1 = cities.FindCity(linkAsString[0]);
+                    City city2 = cities.FindCity(linkAsString[1]);
+
+                    // only add links, where the cities are found 
+                    if ((city1 != null) && (city2 != null))
+                    {
+                        routes.Add(new Link(city1, city2, city1.Location.Distance(city2.Location),
+                                                   TransportModes.Rail));
+                    }
+                }
+            }
+
+            return Count;
+
+        }
+
+        public List<Link> FindShortestRouteBetween(string fromCity, string toCity,
+                                        TransportModes mode)
+        {
+            if (RouteRequestEvent != null)
+            {
+                RouteRequestEvent(this, new RouteRequestEventArgs(fromCity, toCity, mode));
+            }
+            //TODO
+            return new List<Link>();
+        }
+
+    }
+}
