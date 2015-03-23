1b2e0f53d6341fb117325be45a727ed40b8c102b
the-s-a-m <s.merki@hotmail.com>
2015-03-23 12:54:03 +0100

    added Lab6 test

---
+++ RoutePlannerTest/Lab06Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerTest
+{
+    [TestClass]
+    [DeploymentItem("data/citiesTestDataLab3.txt")]
+    [DeploymentItem("data/linksTestDataLab3.txt")]
+    public class Lab06Test
+    {
+        [TestMethod]
+        public void TestFindCitiesByTransportMode()
+        {
+            Cities cities = new Cities();
+            cities.ReadCities(@"citiesTestDataLab3.txt");
+            var routes = new Routes(cities);
+            routes.ReadRoutes(@"linksTestDataLab3.txt");
+
+            City[] citiesByMode = routes.FindCities(TransportModes.Rail);
+
+            Assert.AreEqual(11, citiesByMode.Count());
+
+            // there must be no cities
+            City[] emptyCitiesByMode = routes.FindCities(TransportModes.Bus);
+            Assert.AreEqual(0, emptyCitiesByMode.Count());
+
+        }
+    }
+}
