ee2cd7cbbfd7ce9ce9dda31c37ddff016dd70ede
thejp <jp_55555@hotmail.com>
2015-06-01 01:53:46 +0200

    Added more parallel methods!

--- RoutePlannerLib/RoutesFloydWarshall.cs
+++ RoutePlannerLib/RoutesFloydWarshall.cs
@@ -100,21 +100,51 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
 	        return path;
         }
 
-        private void Setup(List<City> cities, List<Link> links) //Note to self: Not parrallelized, because written D values could be cached (not written to RAM -> not available to other threads), while another thread (with another partition) needs this value later on
+        private void Setup(List<City> cities, List<Link> links)
 		{
-	        D = InitializeWeight(cities, links);
-	        P = new City[cities.Count, cities.Count];
+            if (ExecuteParallel)
+            {
+                SetupParallel(cities, links);
+            }
+            else
+            {
+                D = InitializeWeight(cities, links);
+                P = new City[cities.Count, cities.Count];
 
-			for(var k=0; k<cities.Count; k++)
-	            for(var i=0; i<cities.Count; i++)
-	                for(var j=0; j<cities.Count; j++)
-	                    if( D[i,k] != Double.MaxValue && D[k,j] != Double.MaxValue 
-	                     && D[i,k]+D[k,j] < D[i,j] )
-	                    {
-	                        D[i,j] = D[i,k]+D[k,j];
-	                        P[i,j] = cities[k];
-	                    }
-	    }
+                for (var k = 0; k < cities.Count; k++)
+                    for (var i = 0; i < cities.Count; i++)
+                        for (var j = 0; j < cities.Count; j++)
+                            if (D[i, k] != Double.MaxValue && D[k, j] != Double.MaxValue
+                                && D[i, k] + D[k, j] < D[i, j])
+                            {
+                                D[i, j] = D[i, k] + D[k, j];
+                                P[i, j] = cities[k];
+                            }
+            }
+		}
+
+        private void SetupParallel(List<City> cities, List<Link> links)
+        {
+            D = InitializeWeightParallel(cities, links);
+            P = new City[cities.Count, cities.Count];
+
+            for (var k = 0; k < cities.Count; k++)
+                Parallel.For(0, cities.Count, i =>
+                {
+                    for (var j = 0; j < cities.Count; j++)
+                        if (D[i, k] != Double.MaxValue && D[k, j] != Double.MaxValue
+                            && D[i, k] + D[k, j] < D[i, j])
+                        {
+                            //1) here i!=k && j!=k holds
+                            // => for all x: D[x,k] and D[k,x] are both unchanged during this iteration of k
+                            // => every D[x,k], D[k,x] is guaranteed to be a valid value (valid meaning: there are no cached but not written values from other threads, which are not visible)
+                            //2) D[i,j] is valid (same def of valid as above) because in this iteration of k the pair (i,j) is only processed by one thread
+                            //3) Because of 2 it is also safe to write D[i,j], P[i,j]
+                            D[i, j] = D[i, k] + D[k, j];
+                            P[i, j] = cities[k];
+                        }
+                });
+        }
 
         private double[,] InitializeWeight(List<City> cities, List<Link> links)
 		{
@@ -136,6 +166,27 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
 	        }
 	        return weight;
 	    }
+        private double[,] InitializeWeightParallel(List<City> cities, List<Link> links)
+        {
+            var weight = new double[cities.Count, cities.Count];
+
+            // initialize with MaxValue
+            Parallel.For(0, cities.Count, i =>
+            {
+                for (int j = 0; j < cities.Count; j++)
+                {
+                    weight[i, j] = Double.MaxValue;
+                }
+            });
+
+            Parallel.ForEach(links, e =>
+            {
+                weight[e.FromCity.Index, e.ToCity.Index] = e.Distance;
+                weight[e.ToCity.Index, e.FromCity.Index] = e.Distance;
+            });
+
+            return weight;
+        }
 
     }
 }
